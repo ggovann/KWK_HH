1ba9f1ae4b883ef182b0b5199597faee387342ff
Grace Govan <ggovan11@outlook.com>
2018-06-28 19:02:12 +0000

    code updated

--- models/questions.rb
+++ models/questions.rb
@@ -1,9 +1,38 @@
-def cookie_chooser(user_value)
- if user_value <4
-    "cranberry"
- elsif user_value <7
-    "trailmix"
- elsif user_value <10
-    "chocolatey"
-end
-end
+# def health_quiz(user_value)
+# if user_value == “female”
+# 	if value == “teen”
+# 		if value<4
+# 		     “female_teen_unhealthy”
+# 			elsif value <7
+# 			       “Female_teen_normal”
+# 		                         elsif value <10
+# 			                    “Female_teen_healthy”
+#
+# 		end
+# 	Elsif value == “adult”
+# 		If value<4
+# 			“female _adult_unhealthy”
+# Elsif value <7
+# 			“Female_adult_normal”
+# 		Elsif value <10
+# 			“Female_adult_healthy”
+# 	End
+# end
+# Elsif user_value == “male”
+# 		If value == “teen”
+# 			If value<4
+# 				“male_teen_unhealthy”
+# 				Elsif value <7
+# 					“male_teen_normal”
+# 					Elsif value <10
+# 						“Male_teen_healthy”
+# 			end
+# Elsif value == “adult”
+# 		If value<4
+# 			“male _adult_unhealthy”
+#     		Elsif value <7
+# 					“male_adult_normal”
+# 						Elsif value <10
+# 							“Male_adult_healthy”
+# 		End
+# 	end
